d1ef07b430a90ea8ad6abfbac92c3319b4a22af8
hasanmelih <hasanmelihkls@gmail.com>
2022-04-22 15:38:18 +0300

    Password Reminder

---
+++ 05_Password_Reminder.py
@@ -0,0 +1,8 @@
+user_name = "Hasan Melih"
+password = "hmk0161."
+your_name = input("Please enter your first name: ")
+
+if your_name == user_name :
+    print(f"Hello, {user_name}! The password is: {password}")
+else :
+    print(f"Hello, {your_name}! See you later.")
